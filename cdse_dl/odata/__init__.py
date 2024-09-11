b69d2440ba334a82795970ed9a5825afe465a022
Tyler <31015976+tylanderson@users.noreply.github.com>
2024-09-10 21:10:58 -0400

    use new attribute endpoint

--- cdse_dl/odata/__init__.py
+++ cdse_dl/odata/__init__.py
@@ -1,3 +1,3 @@
-from .attributes import get_collection_known_attributes, get_known_collections
+from .attributes import get_attribute_type, get_collection_attributes, get_collections
 
-__all__ = ["get_collection_known_attributes", "get_known_collections"]
+__all__ = ["get_collection_attributes", "get_collections", "get_attribute_type"]
--- cdse_dl/odata/attributes.py
+++ cdse_dl/odata/attributes.py
@@ -1,235 +1,49 @@
-from typing import Tuple
+from functools import lru_cache
+from typing import Dict, Optional, Tuple
 
-SENTINEL2_ATTRS = (
-    "endingDateTime",
-    "platformShortName",
-    "beginningDateTime",
-    "productType",
-    "origin",
-    "tileId",
-    "cloudCover",
-    "orbitNumber",
-    "productGroupId",
-    "operationalMode",
-    "processingLevel",
-    "processorVersion",
-    "instrumentShortName",
-    "relativeOrbitNumber",
-    "platformSerialIdentifier",
-)
+import requests
 
-SENTINEL1_ATTRS = (
-    "endingDateTime",
-    "platformShortName",
-    "beginningDateTime",
-    "productType",
-    "origin",
-    "timeliness",
-    "orbitNumber",
-    "productClass",
-    "orbitDirection",
-    "operationalMode",
-    "processingLevel",
-    "swathIdentifier",
-    "instrumentShortName",
-    "relativeOrbitNumber",
-    "polarisationChannels",
-    "platformSerialIdentifier",
-)
-
-SENTINEL1_RTC_ATTRS = (
-    "endingDateTime",
-    "platformShortName",
-    "beginningDateTime",
-    "productType",
-    "authority",
-    "orbitNumber",
-    "orbitDirection",
-    "operationalMode",
-    "processingLevel",
-    "spatialResolution",
-    "instrumentShortName",
-    "relativeOrbitNumber",
-    "polarisationChannels",
-    "platformSerialIdentifier",
-)
-
-SENTINEL5P_ATTRS = (
-    "endingDateTime",
-    "platformShortName",
-    "beginningDateTime",
-    "productType",
-)
-
-SENTINEL6_ATTRS = (
-    "endingDateTime",
-    "platformShortName",
-    "beginningDateTime",
-    "productType",
-    "origin",
-    "source",
-    "authority",
-    "processingDate",
-    "processingCenter",
-    "processorVersion",
-    "platformSerialIdentifier",
-)
-
-SENTINEL3_ATTRS = (
-    "endingDateTime",
-    "platformShortName",
-    "beginningDateTime",
-    "productType",
-    "baselineCollection",
-    "instrumentShortName",
-)
+ATTRIBUTES_ENDPOINT = "https://catalogue.dataspace.copernicus.eu/odata/v1/Attributes"
 
 
-GLOBALMOSAICS_ATTRS = (
-    "processingCenter",
-    "platformShortName",
-    "productType",
-    "beginningDateTime",
-    "endingDateTime",
-)
+@lru_cache(1)
+def get_attribute_info() -> Dict:
+    """Get attribute info from OData.
 
-SMOS_ATTRS = (
-    "authority",
-    "orbitNumber",
-    "productType",
-    "endingDateTime",
-    "orbitDirection",
-    "acquisitionType",
-    "operationalMode",
-    "processingLevel",
-    "processingCenter",
-    "processorVersion",
-    "wrsLongitudeGrid",
-    "beginningDateTime",
-    "platformShortName",
-    "spatialResolution",
-    "instrumentShortName",
-    "nativeProductFormat",
-)
-
-ENVISAT_ATTRS = (
-    "authority",
-    "orbitNumber",
-    "phaseNumber",
-    "productType",
-    "endingDateTime",
-    "processingLevel",
-    "beginningDateTime",
-    "platformShortName",
-    "spatialResolution",
-    "instrumentShortName",
-)
-LANDSAT5_ATTRS = (
-    "authority",
-    "rowNumber",
-    "cloudCover",
-    "pathNumber",
-    "orbitNumber",
-    "productType",
-    "numberOfBands",
-    "endingDateTime",
-    "operationalMode",
-    "processingLevel",
-    "sunAzimuthAngle",
-    "beginningDateTime",
-    "platformShortName",
-    "spatialResolution",
-    "sunElevationAngle",
-    "instrumentShortName",
-)
-
-LANDSAT7_ATTRS = (
-    "authority",
-    "rowNumber",
-    "cloudCover",
-    "pathNumber",
-    "orbitNumber",
-    "productType",
-    "numberOfBands",
-    "endingDateTime",
-    "operationalMode",
-    "processingLevel",
-    "sunAzimuthAngle",
-    "beginningDateTime",
-    "platformShortName",
-    "spatialResolution",
-    "sunElevationAngle",
-    "instrumentShortName",
-)
-
-LANDSAT8_ATTRS = (
-    "authority",
-    "rowNumber",
-    "cloudCover",
-    "pathNumber",
-    "productType",
-    "numberOfBands",
-    "endingDateTime",
-    "processingLevel",
-    "sunAzimuthAngle",
-    "beginningDateTime",
-    "platformShortName",
-    "spatialResolution",
-    "sunElevationAngle",
-    "instrumentShortName",
-)
-
-TERRAAQUA_ATTRS = (
-    "authority",
-    "productType",
-    "endingDateTime",
-    "processingLevel",
-    "beginningDateTime",
-    "platformShortName",
-    "spatialResolution",
-    "instrumentShortName",
-)
-
-S2GLC_ATTRS = ("productType", "endingDateTime", "processorVersion", "beginningDateTime")
-
-COPDEM_ATTRS = (
-    "authority",
-    "productType",
-    "endingDateTime",
-    "beginningDateTime",
-    "spatialResolution",
-    "polarisationChannels",
-)
-
-KNOWN_ATTRS_BY_COLLECTION = {
-    "SENTINEL-1": SENTINEL1_ATTRS,
-    "SENTINEL-2": SENTINEL2_ATTRS,
-    "SENTINEL-3": SENTINEL3_ATTRS,
-    "SENTINEL-5P": SENTINEL5P_ATTRS,
-    "SENTINEL-6": SENTINEL6_ATTRS,
-    "SENTINEL-1-RTC": SENTINEL1_RTC_ATTRS,
-    "GLOBAL-MOSAICS": GLOBALMOSAICS_ATTRS,
-    "SMOS": SMOS_ATTRS,
-    "ENVISAT": ENVISAT_ATTRS,
-    "LANDSAT-5": LANDSAT5_ATTRS,
-    "LANDSAT-7": LANDSAT7_ATTRS,
-    "LANDSAT-8": LANDSAT8_ATTRS,
-    "COP-DEM": COPDEM_ATTRS,
-    "TERRAAQUA": TERRAAQUA_ATTRS,
-    "S2GLC": S2GLC_ATTRS,
-}
+    Returns:
+        Dict: attribute info
+    """
+    return requests.get(ATTRIBUTES_ENDPOINT).json()
 
 
-def get_known_collections() -> Tuple[str, ...]:
+@lru_cache()
+def get_attribute_type(collection: str, attribute_name: str) -> Optional[str]:
+    """Get attribute type from OData from collection and attribute name.
+
+    Args:
+        collection (str): collection name
+        attribute_name (str): attribute name
+
+    Returns:
+        Optional[str]: attribute type
+    """
+    for attr in get_attribute_info()[collection]:
+        if attribute_name == attr["Name"]:
+            return attr["ValueType"]
+
+    return None
+
+
+def get_collections() -> Tuple[str, ...]:
     """Get all known collections.
 
     Returns:
         Tuple[str, ...]: known collections
     """
-    return tuple(KNOWN_ATTRS_BY_COLLECTION.keys())
+    return tuple(get_attribute_info().keys())
 
 
-def get_collection_known_attributes(collection: str) -> Tuple[str, ...]:
+def get_collection_attributes(collection: str) -> Tuple[str, ...]:
     """Get all know attributes that can be queried for a collection.
 
     Args:
@@ -242,6 +56,6 @@ def get_collection_known_attributes(collection: str) -> Tuple[str, ...]:
         Tuple[str, ...]: known attributes
 
     """
-    if collection not in get_known_collections():
+    if collection not in get_collections():
         raise ValueError(f"Invalid Collection: {collection}")
-    return KNOWN_ATTRS_BY_COLLECTION[collection]
+    return tuple(attr["Name"] for attr in get_attribute_info()[collection])
